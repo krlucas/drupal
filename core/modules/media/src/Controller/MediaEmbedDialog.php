0ac1f64f1cfd59532cc5b9371580528ebef2dbc7
Kelly Lucas <krlucas@gmail.com>
2018-01-07 15:31:49 -0500

    Remove hardcoded create args.

--- core/modules/media/src/Controller/MediaEmbedDialog.php
+++ core/modules/media/src/Controller/MediaEmbedDialog.php
@@ -3,13 +3,17 @@
 namespace Drupal\media\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Routing\RequestContext;
 use Drupal\media\Entity\Media;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class MediaEmbedDialog extends ControllerBase {
 
-  public function form() {
-    $entity = Media::create(['bundle' => 'file', 'uid' => 1]);
+  public function form($editor, $media_type) {
+    $entity = Media::create(['bundle' => $media_type, 'uid' => $this->currentUser()->id()]);
     $form = $this->entityFormBuilder()->getForm($entity, 'editor_embed');
     return $form;
   }
+
 }
